2e44bb7e76d7af401a1b34885929a761478cb74b
wesleywu55 <wesleywu5555@gmail.com>
2026-05-13 20:56:01 +0800

    feat(product): 利用文字插值顯示產品名稱、作者與公司資訊

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
   templateUrl: './product-card.component.html',
   styleUrl: './product-card.component.scss',
 })
-export class ProductCardComponent {}
+export class ProductCardComponent {
+  protected productName = 'A 產品';
+  protected author = '作者A、作者B、作者C';
+  protected company = '博碩文化';
+}
