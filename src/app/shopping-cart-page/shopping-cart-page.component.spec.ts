b01e48dd83974a4fe16d3e8cb0b20200cd5bdb31
wesleywu55 <wesleywu5555@gmail.com>
2026-05-20 15:30:15 +0800

    feat(shopping-cart-page): 新增購物車頁面元件

---
+++ src/app/shopping-cart-page/shopping-cart-page.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ShoppingCartPageComponent } from './shopping-cart-page.component';
+
+describe('ShoppingCartPageComponent', () => {
+  let component: ShoppingCartPageComponent;
+  let fixture: ComponentFixture<ShoppingCartPageComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ShoppingCartPageComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(ShoppingCartPageComponent);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/shopping-cart-page/shopping-cart-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shopping-cart-page',
+  imports: [],
+  templateUrl: './shopping-cart-page.component.html',
+  styleUrl: './shopping-cart-page.component.scss',
+})
+export class ShoppingCartPageComponent {}
