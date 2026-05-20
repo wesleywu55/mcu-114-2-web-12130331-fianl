b2973f97150728daf6b8b72ba8a34e326b7fc74a
wesleywu55 <wesleywu5555@gmail.com>
2026-05-21 00:00:29 +0800

    feat(product): 實作產品資料分頁作業

--- src/app/product-card-list/product-card-list.component.ts
+++ src/app/product-card-list/product-card-list.component.ts
@@ -1,11 +1,10 @@
-import { Component, input, output, signal } from '@angular/core';
+import { Component, input, output } from '@angular/core';
 import { ProductCardComponent } from '../product-card/product-card.component';
 import { Product } from '../model/product';
-import { PaginationComponent } from '../pagination/pagination.component';
 
 @Component({
   selector: 'app-product-card-list',
-  imports: [PaginationComponent, ProductCardComponent],
+  imports: [ProductCardComponent],
   templateUrl: './product-card-list.component.html',
   styleUrl: './product-card-list.component.scss',
 })
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -46,9 +46,44 @@ export class ProductService {
       photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
       price: 15800,
     }),
+    new Product({
+      id: 6,
+      name: 'F 產品',
+      authors: ['作者A', '作者B', '作者C'],
+      company: '博碩文化',
+      photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
+      price: 15800,
+    }),
+    new Product({
+      id: 7,
+      name: 'G 產品',
+      authors: ['作者A', '作者B', '作者C'],
+      company: '博碩文化',
+      photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
+      price: 15800,
+    }),
+    new Product({
+      id: 8,
+      name: 'H 產品',
+      authors: ['作者A', '作者B', '作者C'],
+      company: '博碩文化',
+      photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
+      price: 15800,
+    }),
+    new Product({
+      id: 9,
+      name: 'I 產品',
+      authors: ['作者A', '作者B', '作者C'],
+      company: '博碩文化',
+      photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
+      price: 15800,
+    }),
   ];
 
-  getList(): Product[] {
-    return this._data;
+  getList(name: string | undefined, index: number, size: number): { data: Product[]; count: number } {
+    const startIndex = (index - 1) * size;
+    const endIndex = startIndex + size;
+    const data = name ? this._data.filter((item) => item.name === name) : [...this._data];
+    return { data: data.slice(startIndex, endIndex), count: this._data.length };
   }
 }
