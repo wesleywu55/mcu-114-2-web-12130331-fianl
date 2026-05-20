dbbab788b811883f250f07c2120f3dbf0589ab77
wesleywu55 <wesleywu5555@gmail.com>
2026-05-20 15:37:22 +0800

    feat(router): 切換產品清單與購物車頁面實作

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { ProductPageComponent } from './product-page/product-page.component';
+import { ShoppingCartPageComponent } from './shopping-cart-page/shopping-cart-page.component';
+
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'products' },
+  { path: 'products', component: ProductPageComponent },
+  { path: 'shopping-cart', component: ShoppingCartPageComponent },
+];
