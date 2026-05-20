ba721e04d8c3c55654874ea2233b927efe42222f
wesleywu55 <wesleywu5555@gmail.com>
2026-05-21 01:32:47 +0800

    refactor(product): 利用 rx resource 取得與重新資料

--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -1,9 +1,10 @@
-import { Component, effect, inject, OnInit, signal } from '@angular/core';
+import { Component, computed, effect, inject, OnInit, signal } from '@angular/core';
 import { Product } from '../model/product';
 import { ProductCardListComponent } from '../product-card-list/product-card-list.component';
 import { Router } from '@angular/router';
 import { ProductService } from '../services/product.service';
 import { PaginationComponent } from '../pagination/pagination.component';
+import { rxResource } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-product-page',
@@ -20,26 +21,26 @@ export class ProductPageComponent {
 
   protected readonly pageSize = signal(5);
 
-  protected readonly totalCount = signal(0);
-
-  protected readonly products = signal<Product[]>([]);
-
-  constructor() {
-    effect(() => {
-      const pageIndex = this.pageIndex();
-      const pageSize = this.pageSize();
-      this.getProducts(pageIndex, pageSize);
-    });
-  }
-
   onView(product: Product): void {
     this.router.navigate(['product', product.id]);
   }
 
-  private getProducts(pageIndex: number, pageSize: number): void {
-    this.productService.getList(undefined, pageIndex, pageSize).subscribe(({ data, count }) => {
-      this.products.set(data);
-      this.totalCount.set(count);
-    });
-  }
+  private readonly data = rxResource({
+    params: () => ({ pageIndex: this.pageIndex(), pageSize: this.pageSize() }),
+    defaultValue: { data: [], count: 0 },
+    stream: ({ params }) => {
+      const { pageIndex, pageSize } = params;
+      return this.productService.getList(undefined, pageIndex, pageSize);
+    },
+  });
+
+  protected readonly totalCount = computed(() => {
+    const { count } = this.data.value();
+    return count;
+  });
+
+  protected readonly products = computed(() => {
+    const { data } = this.data.value();
+    return data;
+  });
 }
