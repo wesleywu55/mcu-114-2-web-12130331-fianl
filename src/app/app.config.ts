8fef6273c92ec1a7be8802c40a64a3277afa31ce
wesleywu55 <wesleywu5555@gmail.com>
2026-05-21 01:59:16 +0800

    feat(product): 實作從後端服務取得所有產品資料

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,15 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { ProductService } from './services/product.service';
+import { ProductRemoteService } from './services/product-remote.service';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes, withComponentInputBinding())],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideHttpClient(),
+    provideRouter(routes, withComponentInputBinding()),
+    { provide: ProductService, useClass: ProductRemoteService },
+  ],
 };
